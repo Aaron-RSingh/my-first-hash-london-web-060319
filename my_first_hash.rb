c77969c81ebe539ace4eab90f88ee93be015c2bc
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-30 08:59:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_hash = {"my_name" => "Aaron", "my_age" => "24"}
+my_hash = {"my name" => "Aaron", "my age" => "24"}
   
 end
 
